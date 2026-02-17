6761722eec346bfa70b3ea37dabfb6d918ed9a2b
Anastasiia Rusakova <arusakova917@gmail.com>
2026-02-17 19:45:41 +0200

    Fixed readme and test with Ketrin and different tops

--- test_tryon_v1.6.py
+++ test_tryon_v1.6.py
@@ -12,10 +12,11 @@ client = Fashn(
 result = client.predictions.subscribe(
     model_name="tryon-v1.6",
     inputs={
+        "segmentation_free": "false",
         "mode": "quality",
         "garment_photo_type": "model",
-        "garment_image": "https://static.wfolio.com/file/AqiFFw_TXMM4LDwoI2TPSVbk9gd-1s7x/yoh_TtDt5eGETeyieELbAqr824BKVSQo/dP01UUGVgf-fgyPoMQWSZ4WSj-w98dcK/Yan_NthmIn2e-46cooadKJdepOqsK9wl/2sBL_s5UW2nM0YrAnsp9sjQszFbdcEKr/g_vZ7rkjKF0.jpeg",
-        "model_image": "https://static.wfolio.com/file/AqiFFw_TXMM4LDwoI2TPSVbk9gd-1s7x/yoh_TtDt5eGETeyieELbAqr824BKVSQo/k7zJHkA3dTe8xyBXY2GeblnfF5ot8Xt1/7F403DIlaygCwvDaFD7-OOcGzcJ4jqrh/cYZ4xX89Q6pG3mcdNGJCIv9fzVVn4SB2/1569W2e_IS0.png",
+        "garment_image": "https://static.wfolio.com/file/AqiFFw_TXMM4LDwoI2TPSVbk9gd-1s7x/yoh_TtDt5eGETeyieELbAqr824BKVSQo/fMeHlVjqkKHa10hdt8jvgnGMk1kmwnWh/Un70_M5qhnyfpuDvi0SyfXel_QMjLt90/R27Qf964AOLoIhXCMHzs7Dnlt-gFS7_R/qFxTV2DStcA.jpeg",
+        "model_image": "https://static.wfolio.com/file/AqiFFw_TXMM4LDwoI2TPSVbk9gd-1s7x/yoh_TtDt5eGETeyieELbAqr824BKVSQo/ywCqmKO9wl5hgf3SX5C5q77wqIDTdBHR/E3fWXJIAynPaNH2M-GnrBRIXUN9GjDTa/I_UKgKr_rF_NHqtb01IFfwRc0yzaGDnu/fk1TnQz9IqQ.jpg",
         "category": 'tops'
     },
 )
